2f4c42c78955024d10206078b6d20267cee2d9b8
duongtavan94 <duongtavan94@gmail.com>
2018-04-04 00:00:37 +0700

    blade tra cuu

--- resources/views/Frontend/SinhVien/TraCuu.blade.php
+++ resources/views/Frontend/SinhVien/TraCuu.blade.php
@@ -6,5 +6,10 @@ Tra cứu sinh viên
 <div class="col-xs-12 content-1">
     <div class="col-xs-12 gioithieu-1">Tra cứu sinh viên</div>
     <div class="col-xs-12 tracuu1">Nhập thông tin</div>
+    <div class="col-xs-12 tracuu2">Họ và tên :</div>
+    <div class="col-xs-12 tracuu2"><input id="name" type="text" class="form-control" name=""></div>
+    <div class="col-xs-12 tracuu2">Mã sinh viên :</div>
+    <div class="col-xs-12 tracuu2"><input id="name" type="text" class="form-control" name=""></div>
+    <div class="col-xs-12 tracuu2"><button class="btn btn-primary">Tìm kiếm</button></div>
 </div>
 @endsection
